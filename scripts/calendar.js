5910c41fe3ff353cc5334d92afab5fed7ad7a55a
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-07-29 17:11:57 -0400

    commit

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,39 +1,42 @@
-$('#player_button').click(function(){
-  var clicks = $(this).data('clicks_player_button');
-  if (!clicks) {
-    $('#player_button .button_menu').css({overflow: 'visible', height: 'auto'})
-    $('#player_button .button_menu a').css({display: 'flex'})
-    
-    anime({
-      targets: '#player_button .button_menu a',
-      opacity: 1,
-      duration: 100,
-      easing: 'easeInQuad'
-    })
-
-  } else {
-    $('#player_button .button_menu').css({overflow: 'hidden', height: 0})
-    $('#player_button .button_menu a').css({display: 'flex', opacity: 0})
-  }
-  $(this).data("clicks_player_button", !clicks);
+$('#player_button').focusout(function() {
+  window.setTimeout(function() {
+    $('#player_button .button_content > span').css({opacity: 1});
+    $('#player_button .button_menu').css({opacity: 0});
+    $('#player_button .button_menu').css({display: 'none'});
+  }, 200)
 })
 
-$('#details_button').click(function(){
-  var clicks = $(this).data('clicks_details_button');
-  if (!clicks) {
-    $('#details_button .button_menu').css({overflow: 'visible', height: 'auto'})
-    $('#details_button .button_menu a').css({display: 'flex'})
-    
-    anime({
-      targets: '#details_button .button_menu a',
-      opacity: 1,
-      duration: 100,
-      easing: 'easeInQuad'
-    })
+$('#player_button').focus(function(){
+  $('#player_button .button_content > span').css({opacity: 0});
+  $('#player_button .button_menu').css({display: 'flex'});
+  
+  anime({
+    targets: '#player_button .button_menu',
+    opacity: 1,
+    duration: 100,
+    delay: 200,
+    easing: 'easeInQuad'
+  })
+})
 
-  } else {
-    $('#details_button .button_menu').css({overflow: 'hidden', height: 0})
-    $('#details_button .button_menu a').css({display: 'flex', opacity: 0})
-  }
-  $(this).data("clicks_details_button", !clicks);
+
+$('#details_button').focusout(function() {
+  window.setTimeout(function() {
+    $('#details_button .button_content > span').css({opacity: 1});
+    $('#details_button .button_menu').css({opacity: 0});
+    $('#details_button .button_menu').css({display: 'none'});
+  }, 200)
+})
+
+$('#details_button').focus(function(){
+  $('#details_button .button_content > span').css({opacity: 0});
+  $('#details_button .button_menu').css({display: 'flex'});
+
+  anime({
+    targets: '#details_button .button_menu',
+    opacity: 1,
+    duration: 100,
+    delay: 200,
+    easing: 'easeInQuad'
+  })
 })
