920d743f1329e35152a843dcd74ca1027bc6bbc8
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-08-03 17:06:57 -0400

    finito

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -1,4 +1,5 @@
-$('#player_button').focusout(function() {
+$('#player_button').mouseleave(function() {
+  $(this).data('clicks', false);
   window.setTimeout(function() {
     $('#player_button .button_content > span').css({opacity: 1});
     $('#player_button .button_menu').css({opacity: 0});
@@ -6,21 +7,32 @@ $('#player_button').focusout(function() {
   }, 200)
 })
 
-$('#player_button').focus(function(){
-  $('#player_button .button_content > span').css({opacity: 0});
-  $('#player_button .button_menu').css({display: 'flex'});
-  
-  anime({
-    targets: '#player_button .button_menu',
-    opacity: 1,
-    duration: 100,
-    delay: 200,
-    easing: 'easeInQuad'
-  })
+$('#player_button').click(function(){
+  var clicks = $(this).data('clicks');
+  if (!clicks) {
+    $('#player_button .button_content > span').css({opacity: 0});
+    $('#player_button .button_menu').css({display: 'flex'});
+    
+    anime({
+      targets: '#player_button .button_menu',
+      opacity: 1,
+      duration: 100,
+      delay: 200,
+      easing: 'easeInQuad'
+    })
+ } else {
+    window.setTimeout(function() {
+      $('#player_button .button_content > span').css({opacity: 1});
+      $('#player_button .button_menu').css({opacity: 0});
+      $('#player_button .button_menu').css({display: 'none'});
+    }, 200)
+ }
+ $(this).data("clicks", !clicks);
 })
 
 
-$('#details_button').focusout(function() {
+$('#details_button').mouseleave(function() {
+  $(this).data('clicks', false);
   window.setTimeout(function() {
     $('#details_button .button_content > span').css({opacity: 1});
     $('#details_button .button_menu').css({opacity: 0});
@@ -28,15 +40,25 @@ $('#details_button').focusout(function() {
   }, 200)
 })
 
-$('#details_button').focus(function(){
-  $('#details_button .button_content > span').css({opacity: 0});
-  $('#details_button .button_menu').css({display: 'flex'});
+$('#details_button').click(function(){
+  var clicks = $(this).data('clicks');
+  if (!clicks) {
+    $('#details_button .button_content > span').css({opacity: 0});
+    $('#details_button .button_menu').css({display: 'flex'});
 
-  anime({
-    targets: '#details_button .button_menu',
-    opacity: 1,
-    duration: 100,
-    delay: 200,
-    easing: 'easeInQuad'
-  })
+    anime({
+      targets: '#details_button .button_menu',
+      opacity: 1,
+      duration: 100,
+      delay: 200,
+      easing: 'easeInQuad'
+    })
+ } else {
+    window.setTimeout(function() {
+      $('#details_button .button_content > span').css({opacity: 1});
+      $('#details_button .button_menu').css({opacity: 0});
+      $('#details_button .button_menu').css({display: 'none'});
+    }, 200)
+ }
+ $(this).data("clicks", !clicks);
 })
--- scripts/details.js
+++ scripts/details.js
@@ -13,18 +13,47 @@ $("#tab").one("mousedown", function() {
     cancel: ".details__date__detes__basic, .details__date__detes__countdown",
     containment: "parent"
   });
+  $( ".details__date__detes .content" ).draggableTouch({
+    axis: "x",
+    cancel: ".details__date__detes__basic, .details__date__detes__countdown",
+    containment: "parent"
+  });
+});
+$("#tab").one("touchstart", function() {
+  $( ".details__date__detes .content" ).draggable({
+    axis: "x",
+    cancel: ".details__date__detes__basic, .details__date__detes__countdown",
+    containment: "parent"
+  });
+  $( ".details__date__detes .content" ).draggableTouch({
+    axis: "x",
+    cancel: ".details__date__detes__basic, .details__date__detes__countdown",
+    containment: "parent"
+  });
 });
 
-$("#tab").mouseup(function() {
+function tap() {
   let t = $('.details__date__detes .content');
   $('.details__date__detes #tab').css("cursor", "grab");
   if(parseInt(t.css("left")) <= t.width() / 2 && !t.hasClass("ui-draggable-dragging")) {
     t.removeClass('over');
     t.css("left", $(".details__date__detes__basic").outerWidth());
+    window.setTimeout(function() {
+      t.find("#tab span").text("CLOSE ME");
+    }, 200)
+
   } else if(parseInt(t.css("left")) >= t.width() / 2 && !t.hasClass("ui-draggable-dragging")) {
     t.css("left", "0");
+    window.setTimeout(function() {
+      t.find("#tab span").text("CLICK ME");
+    }, 200)
+    
   }
-})
+}
+
+$("#tab").mouseup(tap)
+
+$("#tab").bind("touchend", tap)
 
 $("#tab").mousedown(function() {
   $('.details__date__detes #tab').css("cursor", "grabbing");
