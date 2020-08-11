0a303bccaf3ae6204916b6390db075f2f9dec45b
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-08-11 18:02:11 -0400

    change slugs

--- scripts/language.js
+++ scripts/language.js
@@ -6,9 +6,9 @@ var URLparam = new URLSearchParams(window.location.search);
 
 if (URLparam.get("tran") != "0") {
   if (en) {
-    window.location.href = "/index.html?tran=0";
+    window.location.href = "/index?tran=0";
   } else {
-    window.location.href = "/index_spanish.html?tran=0";
+    window.location.href = "/index_spanish?tran=0";
   }
 }
 
