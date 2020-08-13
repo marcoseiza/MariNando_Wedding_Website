902542dda1eb4cbc4aa9a4bf7f1b59240ee1fbf9
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-08-13 00:14:43 -0400

    fix sizing issue rsvp form, fix text visibility home

--- scripts/details.js
+++ scripts/details.js
@@ -7,41 +7,49 @@ $('#tab').hover(function() {
   $(".details__date__detes .content").removeClass('over');
 })
 
-$("#tab").one("mousedown", function() {
-  $( ".details__date__detes .content" ).draggable({
-    axis: "x",
-    cancel: ".details__date__detes__basic, .details__date__detes__countdown",
-    containment: "parent"
-  });
-});
-$("#tab").one("touchstart", function() {
-  $("#tab").mouseout(tap)
-});
+function close() {
+  let t = $('.details__date__detes .content');
+  t.css("left", "0");
+  window.setTimeout(function() {
+    t.find("#tab span").text("CLICK ME");
+  }, 200) 
+}
 
 function tap() {
   let t = $('.details__date__detes .content');
-  $('.details__date__detes #tab').css("cursor", "grab");
-  if(parseInt(t.css("left")) <= t.width() / 2 && !t.hasClass("ui-draggable-dragging")) {
+
+  if(parseInt(t.css("left")) <= t.width() / 2) {
+
     t.removeClass('over');
     t.css("left", $(".details__date__detes__basic").outerWidth());
+
     window.setTimeout(function() {
       t.find("#tab span").text("CLOSE ME");
     }, 200)
 
-  } else if(parseInt(t.css("left")) >= t.width() / 2 && !t.hasClass("ui-draggable-dragging")) {
+  } else if(parseInt(t.css("left")) >= t.width() / 2) {
+
     t.css("left", "0");
+
     window.setTimeout(function() {
       t.find("#tab span").text("CLICK ME");
-    }, 200)
-    
+      t.blur()
+    }, 200) 
   }
 }
-$("#tab").mouseup(tap)
 
-$("#tab").bind("touchend", tap)
 
-$("#tab").mousedown(function() {
-  $('.details__date__detes #tab').css("cursor", "grabbing");
-})
+$('html').click(close);
+$('#tab').click(function(e){
+  e.stopPropagation();
+  tap()
+});
+
+$('html').on('touchstart', close)
+$(".tab").on('touchstart',function(e) {
+  e.stopPropagation();
+  tap()
+});
+
 
 
--- scripts/language.js
+++ scripts/language.js
@@ -2,13 +2,13 @@ let lang = window.navigator.userLanguage || window.navigator.language,
     en = (lang.includes("en"))? true: (lang.includes("es"))? false : true;
 
 var URLparam = new URLSearchParams(window.location.search);
-
+console.log(URLparam.toString())
 
 if (URLparam.get("tran") != "0") {
   if (en) {
-    window.location.href = "/index?tran=0";
+    window.location.href = "/?" + URLparam.toString() + "&tran=0";
   } else {
-    window.location.href = "/spanish?tran=0";
+    window.location.href = "/spanish?" + URLparam.toString() + "&tran=0";
   }
 }
 
