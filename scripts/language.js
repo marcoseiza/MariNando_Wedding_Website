7e0b97ae2662befde1ea8fa866812bb5c2952f84
Marcos Eizayaga <marcoseizayaga@gmail.com>
2020-08-13 15:11:29 -0400

    install spanish main

--- scripts/language.js
+++ scripts/language.js
@@ -7,7 +7,7 @@ if (URLparam.get("tran") != "0") {
   if (en) {
     window.location.href = "/?" + URLparam.toString() + "&tran=0";
   } else {
-    window.location.href = "/spanish?" + URLparam.toString() + "&tran=0";
+    window.location.href = "/spanish.html?" + URLparam.toString() + "&tran=0";
   }
 }
 
